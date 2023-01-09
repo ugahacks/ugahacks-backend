62be81c1bdb0f731b13ad3bbe10ce066aec8a4c2
Genki <genkima9@gmail.com>
2023-01-08 21:56:05 -0500

    Created yarn workspaces

---
+++ projects/mybyte/pages/resetPasswordSuccess.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ResetPasswordSuccess() {
+  return (
+    <div>
+        <h1>Password reset link has been sent to your email!</h1>
+    </div>
+  )
+}
