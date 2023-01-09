a42016a8f763f75d673e9c017fb0257881c90a3f
Genki <genkima9@gmail.com>
2023-01-09 03:12:27 -0500

    Remove card footer

--- projects/mybyte/components/Event.tsx
+++ projects/mybyte/components/Event.tsx
@@ -49,13 +49,6 @@ export default function Event(props: EventDetail) {
                     {props.description}
                 </Typography>
             </CardBody>
-            <CardFooter divider className="flex items-center justify-between py-3">
-                <Typography variant="small">$899/night</Typography>
-                <Typography variant="small" color="gray" className="flex gap-1">
-                <i className="fas fa-map-marker-alt fa-sm mt-[3px]" />
-                Barcelona, Spain
-                </Typography>
-            </CardFooter>
         </Card>
         </div>
     )
