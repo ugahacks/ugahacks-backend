ba52039b64f09a3f2c6eebc528a5bd08ee55cd08
Genki Aikawa <ga07600@uga.edu>
2023-01-12 23:43:03 -0500

    Changed login and signup page text color

--- projects/mybyte/pages/login.tsx
+++ projects/mybyte/pages/login.tsx
@@ -147,7 +147,7 @@ const LoginPage = () => {
             <div className="flex justify-center pt-8">
               <button
                 type="submit"
-                className={`h-12 text-center w-2/3 bg-[#DC4141] border-2 rounded-md hover:shadow-lg hover:bg-black-800 text-lg transition`}
+                className={`h-12 text-center w-2/3 bg-[#DC4141] border-2 rounded-md hover:shadow-lg hover:bg-gray-800 text-lg transition`}
               >
                 <p className="capitalize text-white font-normal">submit</p>
               </button>
