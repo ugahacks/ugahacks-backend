913b1fcdceacfe547cfc8f3847a133b525bada84
jtm13 <74789040+jtm13@users.noreply.github.com>
2023-01-28 20:45:35 -0500

    Made minor change

--- projects/mybyte/pages/team.tsx
+++ projects/mybyte/pages/team.tsx
@@ -34,7 +34,7 @@ export default function Team() {
             else setPTeams(t);
         };
         getInfo();
-    }, [setTeam, getTeam]);
+    }, [setTeam, getTeam, getPotentialTeams]);
 
     const router = useRouter();
 
@@ -80,7 +80,7 @@ export default function Team() {
             </div>
         </>);
 
-    const methods = useForm<MemberType>({ mode: "onBlur" });
+    const methods = useForm<MemberType>({ mode: "onSubmit" });
 
     const {
         register,
@@ -125,57 +125,94 @@ export default function Team() {
         return true;
     }
 
+    const illuminated = ["block mb-3 font-sans text-black", "block mb-3 font-sans text-[#DC4141]"]
+
     const hasTeam = (
-        <div className="sign-up-form container mx-auto w-96 mt-12 border-2 border-gray-400 overflow-auto bg-white rounded-md">
-            <h2 className="px-12 mt-8 text-center text-xl">
+        <div className="sign-up-form container mx-auto w-96 mt-6 border-2 border-gray-400 overflow-auto bg-white rounded-2xl">
+            <h2 className="px-12 mt-8 text-center text-2xl font-semibold text-[#DC4141]">
                 Your Team
             </h2>
 
-            <div className="mt-8 mb-8">
+            <div className="mt-8 mb-2">
                 <div className="flex justify-center text-xl">
                     <FormProvider {...methods}>
-                        <form action="" className="" onSubmit={handleSubmit(onSubmit)}>
+                        <form 
+                            action="" 
+                            className="w-80 mx-auto pb-6 px-4" 
+                            onSubmit={handleSubmit(onSubmit)}>
+                            
+                            <div className="flex items-center justify-between">
+                              <label htmlFor="" className={illuminated[ + (team.members.length > 0)]}>
+                                Member 1
+                              </label>
+                            </div>
                             <input type="email"
                                 {...register("member1", {
                                     validate: validateEmail,
                                     disabled: (team.members.length > 0),
                                 })}
                                 placeholder={(team.members.length > 0) ? team.members[0] : "Member 1"}
-                                readOnly={(team.members.length > 0)}/>
+                                readOnly={(team.members.length > 0)}
+                                className="border border-solid rounded-lg ring:0 focus:ring-0 focus:outline-none border-gray-400 text-gray-500 text-normal py-3 h-12 px-6 text-lg w-full flex items-center"/>
                             {errors.member1 && (
                               <p className="text-red-400">{errors.member1.message}</p>
                             )}
+                            
+                            <div className="flex items-center justify-between">
+                              <label htmlFor="" className={illuminated[ + (team.members.length > 1)]}>
+                                Member 2
+                              </label>
+                            </div>
                             <input type="email"
                                 {...register("member2", {
                                     validate: validateEmail,
                                     disabled: (team.members.length > 1),
                                 })}
                                 placeholder={(team.members.length > 1) ? team.members[1] : "Member 2"}
-                                readOnly={(team.members.length > 1)}/>
+                                readOnly={(team.members.length > 1)}
+                                className="border border-solid rounded-lg ring:0 focus:ring-0 focus:outline-none border-gray-400 text-gray-500 text-normal py-3 h-12 px-6 text-lg w-full flex items-center"/>
                             {errors.member2 && (
                               <p className="text-red-400">{errors.member2.message}</p>
                             )}
+                            
+                            <div className="flex items-center justify-between">
+                              <label htmlFor="" className={illuminated[ + (team.members.length > 2)]}>
+                                Member 3
+                              </label>
+                            </div>
                             <input type="email"
                                 {...register("member3", {
                                     validate: validateEmail,
                                     disabled: (team.members.length > 2),
                                 })}
                                 placeholder={(team.members.length > 2) ? team.members[2] : "Member 3"}
-                                readOnly={(team.members.length > 2)}/>
+                                readOnly={(team.members.length > 2)}
+                                className="border border-solid rounded-lg ring:0 focus:ring-0 focus:outline-none border-gray-400 text-gray-500 text-normal py-3 h-12 px-6 text-lg w-full flex items-center"/>
                             {errors.member3 && (
                               <p className="text-red-400">{errors.member3.message}</p>
                             )}
+                            
+                            <div className="flex items-center justify-between">
+                              <label htmlFor="" className={illuminated[ + (team.members.length > 3)]}>
+                                Member 4
+                              </label>
+                            </div>
                             <input type="email"
                                 {...register("member4", {
                                     validate: validateEmail,
                                     disabled: (team.members.length > 3),
                                 })}
                                 placeholder={(team.members.length > 3) ? team.members[3] : "Member 4"}
-                                readOnly={(team.members.length > 3)}/>
+                                readOnly={(team.members.length > 3)}
+                                className="border border-solid rounded-lg ring:0 focus:ring-0 focus:outline-none border-gray-400 text-gray-500 text-normal py-3 h-12 px-6 text-lg w-full flex items-center"/>
                             {errors.member4 && (
                               <p className="text-red-400">{errors.member4.message}</p>
                             )}
-                            <input type="submit"></input>
+                            <div className="flex justify-center pt-8">
+                                <button className="h-12 text-center w-2/3 bg-[#DC4141] border-2 rounded-md hover:shadow-lg hover:bg-gray-800 text-lg transition" type="submit">
+                                    <p className="text-white font-normal">Submit</p>
+                                </button>
+                            </div>
                         </form>
                     </FormProvider>
                 </div>
