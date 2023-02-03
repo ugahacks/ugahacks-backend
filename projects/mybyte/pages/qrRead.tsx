4895da3a0e0962392965b620950565721ed7c9a5
jtm13 <74789040+jtm13@users.noreply.github.com>
2023-02-03 13:27:52 -0500

    Commit fixes

--- projects/mybyte/pages/qrRead.tsx
+++ projects/mybyte/pages/qrRead.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { Fragment, useRef, useState } from 'react';
 import Html5QrcodePlugin from '../components/Html5QrcodePlugin';
 import OrganizerRoute from '../components/OrganizerRoute';
 import { useAuth } from '../context/AuthContext';
@@ -6,6 +6,7 @@ import { useAuth } from '../context/AuthContext';
 export default function QrRead(props: any) {
   const {givePoints, checkIn} = useAuth();
   const [data, setData] = useState('No result');
+  const ref = React.useRef<Html5QrcodePlugin | null>(null);
 
   const selectWhich: JSX.Element = (
     <select id="what-for">
@@ -19,24 +20,32 @@ export default function QrRead(props: any) {
   const determineAction = (uid: string) => {
     // action state kept resetting to its default state for some reason
     const val = document.getElementsByTagName("select").namedItem("what-for")?.value;
+    const callback = () => {
+      if (ref != null && ref != undefined) {
+        ref.current?.html5QrcodeScanner?.pause();
+        window.alert("Success! Click to unpause.");
+        ref.current?.html5QrcodeScanner?.resume();
+      }
+    }
     switch(val) {
         case "give-1":
-            givePoints(uid, 1);
+            givePoints(uid, 1).then(callback);
             break;
         case "give-2":
-            givePoints(uid, 2);
+            givePoints(uid, 2).then(callback);
             break;
         case "give-3":
-            givePoints(uid, 3);
+            givePoints(uid, 3).then(callback);
             break;
         case "check-in":
         default:
-            checkIn(uid);
+            checkIn(uid).then(callback);
     }
   }
   return (
     <OrganizerRoute>
       <Html5QrcodePlugin
+        ref={ref}
         fps={10}
         qrbox={250}
         disableFlip={false}
