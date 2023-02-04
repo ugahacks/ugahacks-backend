84e0d3217b65f6d31089c202d7cf3e9dc4da84e9
Genki Aikawa <ga07600@uga.edu>
2023-02-03 21:57:19 -0500

    QR Code point system

--- projects/mybyte/pages/qrRead.tsx
+++ projects/mybyte/pages/qrRead.tsx
@@ -1,47 +1,51 @@
-import React, { Fragment, useRef, useState } from 'react';
-import Html5QrcodePlugin from '../components/Html5QrcodePlugin';
-import OrganizerRoute from '../components/OrganizerRoute';
-import { useAuth } from '../context/AuthContext';
+import React, { Fragment, useRef, useState } from "react";
+import { PassThrough } from "stream";
+import Html5QrcodePlugin from "../components/Html5QrcodePlugin";
+import OrganizerRoute from "../components/OrganizerRoute";
+import { useAuth } from "../context/AuthContext";
 
 export default function QrRead(props: any) {
-  const {givePoints, checkIn} = useAuth();
-  const [data, setData] = useState('No result');
+  const { givePoints, checkIn } = useAuth();
+  const [data, setData] = useState("No result");
   const ref = React.useRef<Html5QrcodePlugin | null>(null);
 
   const selectWhich: JSX.Element = (
     <select id="what-for">
-      <option value="check-in">Check in</option>
-      <option value="give-1">Give 1 point</option>
-      <option value="give-2">Give 2 points</option>
-      <option value="give-3">Give 3 points</option>
+      <option value="side-event">Side Event (500)</option>
+      <option value="company-event">Company Event (1000)</option>
+      <option value="workshop">Workshop (500)</option>
+      <option value="escape-room">Escape Room Winner (1000)</option>
     </select>
   );
 
   const determineAction = (uid: string) => {
     // action state kept resetting to its default state for some reason
-    const val = document.getElementsByTagName("select").namedItem("what-for")?.value;
+    const val = document
+      .getElementsByTagName("select")
+      .namedItem("what-for")?.value;
     const callback = () => {
       if (ref != null && ref != undefined) {
         ref.current?.html5QrcodeScanner?.pause();
         window.alert("Success! Click to unpause.");
         ref.current?.html5QrcodeScanner?.resume();
       }
+    };
+    switch (val) {
+      case "side-event":
+        givePoints(uid, 500).then(callback);
+        break;
+      case "company-event":
+        givePoints(uid, 1000).then(callback);
+        break;
+      case "workshop":
+        givePoints(uid, 500).then(callback);
+        break;
+      case "escape-room":
+        givePoints(uid, 1000).then(callback);
+      default:
+        givePoints(uid, 0).then(callback);
     }
-    switch(val) {
-        case "give-1":
-            givePoints(uid, 1).then(callback);
-            break;
-        case "give-2":
-            givePoints(uid, 2).then(callback);
-            break;
-        case "give-3":
-            givePoints(uid, 3).then(callback);
-            break;
-        case "check-in":
-        default:
-            checkIn(uid).then(callback);
-    }
-  }
+  };
   return (
     <OrganizerRoute>
       <Html5QrcodePlugin
@@ -50,17 +54,17 @@ export default function QrRead(props: any) {
         qrbox={250}
         disableFlip={false}
         qrCodeSuccessCallback={(decodedText: string, decodedResult: any) => {
-            if (data === decodedText) return;
-            if (decodedText.includes("/")) {
-                setData("Not valid User QR-Code");
-                return;
-            } // https://stackoverflow.com/questions/52850099/what-is-the-reg-expression-for-firestore-constraints-on-document-ids
-            setData(decodedText);
-            try {
-                determineAction(decodedText);
-            } catch(error) {
-                console.log(`Something happened: ${error}`);
-            }
+          if (data === decodedText) return;
+          if (decodedText.includes("/")) {
+            setData("Not valid User QR-Code");
+            return;
+          } // https://stackoverflow.com/questions/52850099/what-is-the-reg-expression-for-firestore-constraints-on-document-ids
+          setData(decodedText);
+          try {
+            determineAction(decodedText);
+          } catch (error) {
+            console.log(`Something happened: ${error}`);
+          }
         }}
       />
       <span>{data} </span>
@@ -68,4 +72,4 @@ export default function QrRead(props: any) {
       {selectWhich}
     </OrganizerRoute>
   );
-};
+}
