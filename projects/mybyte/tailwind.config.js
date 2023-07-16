af53920db32d029c3deb16b34fd92079d5934e84
Dhruv Patel <dhruv225@outlook.com>
2023-07-16 19:49:32 -0400

    Extend new font family

--- projects/mybyte/tailwind.config.js
+++ projects/mybyte/tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'inter': ['Inter', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
