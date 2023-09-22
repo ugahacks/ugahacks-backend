bbe6612e395e12581849708321b23dd85459a05c
Kasra Ghaffari <18647702+Kasra-G@users.noreply.github.com>
2023-09-22 12:51:04 -0400

    Fixed 404 page scrolling

--- projects/mybyte/pages/404.tsx
+++ projects/mybyte/pages/404.tsx
@@ -1,10 +1,9 @@
 import Image from "next/image";
 function NotFoundPage() {
     const textStyle = {fontFamily: "Source Code Pro, monospace"}
-    const navbarHeight = 20;    // somehow get the navbar's height programmatically? needed to get rid of the scroll bar
 
     return (
-        <div className={`min-h-screen -mt-${navbarHeight} flex items-center justify-center`}>
+        <div className={`min-h-0 mt-10 flex items-center justify-center`}>
             <div className="text-center">
                 <Image className="mb-5" src="/404.png" alt="not found" height={300} width={300} />
                 <p className="mb-10" style={textStyle}>Where are you, homie?</p>
