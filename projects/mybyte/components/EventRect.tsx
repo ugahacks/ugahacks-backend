0966cc2e461c5e5600b9f019d1b17096878452a6
Justin Moon <jtm88754@gmail.com>
2023-10-03 20:08:55 -0400

    uhacks9 progress

--- projects/mybyte/components/EventRect.tsx
+++ projects/mybyte/components/EventRect.tsx
@@ -29,32 +29,32 @@ export default function EventRect(props: EventDetail) {
     router.push(props.page);
   }
   let statusName: string = "open";
-  let statusClass: string = "border-green text-green";
+  let statusClass: string = "border-green-700 text-green-700";
   let dup = new Date(props.endDate);
   dup.setDate(props.endDate.getDate() + 1);
   if ((new Date()) > dup) {
     statusName = "closed";
-    statusClass = "border-primary text-primary-500";
+    statusClass = "border-primary-500 text-primary-500";
   }
-
+  const dateOpt: Intl.DateTimeFormatOptions = {"year": "numeric", "month": "long", "day": "numeric"};
   return (
     <div onClick={handleClick}>
-      <Card className="w-60 shadow-[#DC4141]">
-        <CardHeader color="blue" className="">
+      <Card className="w-96">
+        <CardHeader color="blue" className="h-28 w-96 mx-0">
           <Image
             src={props.image}
             alt="img-blur-shadow"
+            className="h-full w-60 object-contain"
             fill
-            className="h-full w-full object-contain"
           />
         </CardHeader>
-        <CardBody className="text-center">
-          <Typography variant="h5" className="mb-2 text-bold">
-            Deadline to apply: {props.deadline.toLocaleDateString("default", {"year": "numeric", "month": "long", "day": "numeric"})}
+        <CardBody className="p-0">
+          <Typography variant="h2" className="mb-2 text-bold text-left pl-3 text-sm">
+            Deadline to apply: {props.deadline.toLocaleDateString("default", dateOpt)}
           </Typography>
-          <div className="flex justify-between">
-            <Typography>{props.startDate.toLocaleDateString("default", {"year": "numeric", "month": "long", "day": "numeric"})} - {props.endDate.toLocaleDateString("default", {"year": "numeric", "month": "long", "day": "numeric"})}</Typography>
-            <Typography className={`border ${statusClass}`}>{statusName}</Typography>
+          <div className="flex justify-between mx-3 pb-2 text-xs">
+            <Typography className="text-xs ">{props.startDate.toLocaleDateString("default", dateOpt)} - {props.endDate.toLocaleDateString("default", dateOpt)}</Typography>
+            <Typography className={`text-xs border-2 rounded px-1 ${statusClass}`}>{statusName}</Typography>
           </div>
         </CardBody>
       </Card>
