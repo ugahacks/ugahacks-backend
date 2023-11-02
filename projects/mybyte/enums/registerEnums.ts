2c2bf08fb4b0aca5aee7baf2ea20dbc902f28d5b
Shawn Pradeep <shawnprad@hotmail.com>
2023-11-01 22:36:35 -0400

    Added dietary restrictions opt, to registerEnums

--- projects/mybyte/enums/registerEnums.ts
+++ projects/mybyte/enums/registerEnums.ts
@@ -1,52 +1,61 @@
 export enum ShirtSizes {
-    small = "Small",
-    medium = "Medium",
-    large = "Large",
-    xlarge = "X-Large",
-    xxlarge = "XX-Large",
-    xxxlarge = "XXX-Large"
+  small = "Small",
+  medium = "Medium",
+  large = "Large",
+  xlarge = "X-Large",
+  xxlarge = "XX-Large",
+  xxxlarge = "XXX-Large",
 }
 
 export enum Majors {
-    csci = "Computer Science",
-    csee = "Computer Systems Engineering",
-    cgse = "Cognitive Science",
-    ds = "Data Science",
-    fina = "Finance",
-    mist = "Management Information System",
-    ecse = "Electrical Engineering",
-    mche = "Mechanical Engineering",
-    math = "Mathematics",
-    undecided = "Undecided",
-    other = "Other",
+  csci = "Computer Science",
+  csee = "Computer Systems Engineering",
+  cgse = "Cognitive Science",
+  ds = "Data Science",
+  fina = "Finance",
+  mist = "Management Information System",
+  ecse = "Electrical Engineering",
+  mche = "Mechanical Engineering",
+  math = "Mathematics",
+  undecided = "Undecided",
+  other = "Other",
 }
 
 export enum StudentYears {
-    freshman = "Freshman",
-    sophomore = "Sophomore",
-    junior = "Junior",
-    senior = "Senior",
-    fifth = "5th Year",
-    sixth = "6th Year",
-    masters = "Masters",
-    other = "Other"
+  freshman = "Freshman",
+  sophomore = "Sophomore",
+  junior = "Junior",
+  senior = "Senior",
+  fifth = "5th Year",
+  sixth = "6th Year",
+  masters = "Masters",
+  other = "Other",
 }
 
 export enum Genders {
-    female = "Female",
-    male = "Male",
-    non_binary = "Non-binary",
-    prefer_not_to_say = "Prefer not to say",
+  female = "Female",
+  male = "Male",
+  non_binary = "Non-binary",
+  prefer_not_to_say = "Prefer not to say",
 }
 
 export enum LevelsOfStudy {
-    under2 = "Undergraduate University (2 year - community college or similar)",
-    under3 = "Undergraduate University (3+ year)",
-    grad = "Graduate University (Masters, Professional, Doctoral, etc.)",
-    boot = "Code School / Bootcamp",
-    voc = "Other Vocational / Trade School or Apprenticeship",
-    postDoc = "Post Doctorate",
-    other = "Other",
-    nas = "I am currently not a student",
-    idk = "Prefer not to answer",
+  under2 = "Undergraduate University (2 year - community college or similar)",
+  under3 = "Undergraduate University (3+ year)",
+  grad = "Graduate University (Masters, Professional, Doctoral, etc.)",
+  boot = "Code School / Bootcamp",
+  voc = "Other Vocational / Trade School or Apprenticeship",
+  postDoc = "Post Doctorate",
+  other = "Other",
+  nas = "I am currently not a student",
+  idk = "Prefer not to answer",
+}
+
+export enum DietaryRestrictions {
+  vegetarian = "Vegetarian",
+  vegan = "Vegan",
+  celiacDisease = "Celiac Disease",
+  kosher = "Kosher",
+  halal = "Halal",
+  other = "Other",
 }
