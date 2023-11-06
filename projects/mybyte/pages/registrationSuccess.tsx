5c56adb2510c849748b456b5797da585bf2e00ab
Shawn Pradeep <shawnprad@hotmail.com>
2023-11-06 00:43:56 -0500

    Added Soaring byte, changed layout for confetti test

--- projects/mybyte/pages/registrationSuccess.tsx
+++ projects/mybyte/pages/registrationSuccess.tsx
@@ -1,12 +1,22 @@
 import Link from "next/link";
 import React from "react";
+import Image from "next/image";
 
 export default function RegistrationSuccess() {
   return (
     <div className="sign-up-form container mx-auto w-96 mt-12 border-2 border-gray-400 overflow-auto bg-white rounded-md">
-      <h2 className="px-12 mt-8 text-center text-xl">
-        Congratulations! You have successfully registered for UGAHacks 9! :) 🥳
-      </h2>
+      <div className="flex flex-col items-center">
+        <Image src="/byte soaring.png" alt="circle" width={300} height={300} />
+        <div className="px-12 mt-8 text-center text-xl font-inter">
+          {" "}
+          <h2>
+            <span className="text-3xl">Congratulations!</span>
+          </h2>
+          <h2 className="font-mono text-lg pt-2">
+            You have successfully registered for UGAHacks 9!
+          </h2>
+        </div>
+      </div>
 
       <div className="mt-8 mb-8">
         <div className="flex justify-center text-xl">
