85e8e43dc9ba77c48e9c2a4c8b81b28dc96d8d8b
Shawn Pradeep <shawnprad@hotmail.com>
2023-11-06 01:46:19 -0500

    Added confetti, redesigned layout to fit Figma

--- projects/mybyte/pages/registrationSuccess.tsx
+++ projects/mybyte/pages/registrationSuccess.tsx
@@ -1,28 +1,42 @@
 import Link from "next/link";
 import React from "react";
 import Image from "next/image";
+import ConfettiAnimation from "../components/Confetti";
 
+// TODO: Go back and make this section responsive for smaller widths
 export default function RegistrationSuccess() {
   return (
-    <div className="sign-up-form container mx-auto w-96 mt-12 border-2 border-gray-400 overflow-auto bg-white rounded-md">
-      <div className="flex flex-col items-center">
-        <Image src="/byte soaring.png" alt="circle" width={300} height={300} />
-        <div className="px-12 mt-8 text-center text-xl font-inter">
-          {" "}
-          <h2>
-            <span className="text-3xl">Congratulations!</span>
-          </h2>
-          <h2 className="font-mono text-lg pt-2">
-            You have successfully registered for UGAHacks 9!
-          </h2>
-        </div>
+    <div>
+      <div className="relative">
+        <ConfettiAnimation />
       </div>
 
-      <div className="mt-8 mb-8">
-        <div className="flex justify-center text-xl">
-          <p className="text-cyan-500">
-            <Link href="/dashboard">Return to dashboard</Link>
-          </p>
+      <div className="absolute inset-0 flex pt-16 overflow-hidden font-mono">
+        <div className="sign-up-form container mx-auto max-w-min mt-12 bg-white rounded-md">
+          <div className="flex flex-col items-center">
+            <Image
+              src="/byte soaring.png"
+              alt="circle"
+              width={400}
+              height={400}
+            />
+            <div className="px-12 mt-8 text-center text-xl">
+              {" "}
+              <h1 className="text-6xl">Congratulations!</h1>
+              <h2 className="text-xl pt-4">
+                You have successfully registered for UGAHacks 9!
+              </h2>
+              <h2 className="text-xl">See you soon! 😃</h2>
+            </div>
+          </div>
+
+          <div className="mt-8 mb-8">
+            <div className="flex justify-center text-xl">
+              <p className="text-primary-500 hover:underline underline-offset-2">
+                <Link href="/dashboard">return to dashboard</Link>
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
