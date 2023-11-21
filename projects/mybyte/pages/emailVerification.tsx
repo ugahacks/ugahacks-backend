84983ca6bb8ad2e098995a3c52cf578a6abc2c98
Shawn Pradeep <shawnprad@hotmail.com>
2023-11-21 10:48:57 -0500

    new email verification page

--- projects/mybyte/pages/emailVerification.tsx
+++ projects/mybyte/pages/emailVerification.tsx
@@ -1,20 +1,57 @@
-import Link from "next/link";
 import React from "react";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import greenEmailIcon from "../public/emailVerificationGreenIcon.png";
+import Image from "next/image";
+import Typewriter from "typewriter-effect";
 
 export default function EmailVerification() {
-  return (
-    <div className="sign-up-form container mx-auto w-96 mt-12 border-2 border-gray-400 overflow-auto bg-white rounded-md">
-      <h2 className="px-12 mt-8 text-center text-xl">
-        Thank you for registering with us! Please verify your email to continue.
-        A verification link has been sent to your email.
-      </h2>
+  const router = useRouter();
+  const { email } = router.query;
 
-      <div className="mt-8 mb-8">
-        <div className="flex justify-center text-xl">
-          <p className="text-cyan-500">
-            <Link href="/login">Go to login</Link>
-          </p>
-        </div>
+  const handleClick = () => {
+    router.push("/login");
+  };
+
+  return (
+    <div className="container mx-auto md:w-[600px] text-gray-700 p-4">
+      <div className="flex items-center justify-center mb-4">
+        <Image src={greenEmailIcon} className="w-24" alt="google logo" />
+      </div>
+      <div className="text-6xl font-inter font-bold text-gray-700 mb-4 text-center">
+        <Typewriter
+          onInit={(typewriter) => {
+            typewriter.typeString("Verify your email").start();
+          }}
+          options={{ cursor: "" }}
+        />
+      </div>
+
+      <div className="font-mono mb-10">
+        <p className="text-lg text-center">
+          Thanks for registering an account with us. Please verify your email to
+          continue. A verification link has been sent to{" "}
+          <span className="font-bold">{email}</span>
+        </p>
+      </div>
+
+      <div className="flex justify-center mb-4">
+        <button
+          onClick={handleClick}
+          type="submit"
+          className={`h-10 text-center hover:text-white w-80 bg-[#F8F8F8] rounded-md hover:shadow-lg hover:bg-primary-500 active:bg-primary-950 text-lg transition`}
+        >
+          <p className="font-normal">Return to login</p>
+        </button>
+      </div>
+
+      <div className="mb-4">
+        <p className="text-center text-sm font-mono">
+          Questions? Email us at{" "}
+          <Link href="mailto:tech@ugahacks.com" className="underline">
+            tech@ugahacks.com
+          </Link>
+        </p>
       </div>
     </div>
   );
--- projects/mybyte/pages/signup.tsx
+++ projects/mybyte/pages/signup.tsx
@@ -31,7 +31,8 @@ const SignupPage = () => {
   const onSubmit = async (data: SignupType) => {
     try {
       await signUp(data.first_name, data.last_name, data.email, data.password);
-      router.push("/emailVerification");
+      router.push(`/emailVerification?email=${data.email}`);
+      // router.push("/emailVerification");
     } catch (err: any) {
       if (err instanceof FirebaseError) {
         console.log(err.code);
@@ -76,24 +77,6 @@ const SignupPage = () => {
               <Image src={googleLogo} className="w-5" alt="google logo" />
             </div>
           </button>
-          {/* <button
-            className="group h-12 px-6 border-2 border-gray-300 rounded-full transition duration-300 
- hover:border-blue-400 focus:bg-blue-50 active:bg-blue-100"
-          >
-            <div className="relative flex items-center space-x-4 justify-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                className="absolute left-0 w-5 text-gray-700"
-                viewBox="0 0 16 16"
-              >
-                <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
-              </svg>
-              <span className="block w-max font-semibold tracking-wide text-gray-700 text-sm transition duration-300 group-hover:text-blue-600 sm:text-base">
-                Continue with Github
-              </span>
-            </div>
-          </button> */}
         </div>
         <div className="relative flex py-5 items-center space-x-1">
           <div className="flex-grow border-t border-gray-400"></div>
