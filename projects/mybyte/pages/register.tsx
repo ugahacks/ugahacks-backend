f72c6f6563daf4b07aaebf7d4e1377d70fad9126
Varshini Venkatasamy <varshini@ugahacks.com>
2026-08-09 22:39:24 -0400

    Changes

--- projects/mybyte/pages/register.tsx
+++ projects/mybyte/pages/register.tsx
@@ -259,14 +259,14 @@ export default function Register() {
                   onInit={(typewriter) => {
                     typewriter
                       .typeString("Register for ")
-                      .typeString("UGAHacks 11")
+                      .typeString("the UGA Cadathon")
                       .start();
                   }}
                 />
               </h1>
               <div className="pl-1 text-md w-4/5">
                 <p className="pb-3">
-                  We&apos;re excited that you are participating in UGAHacks 11!
+                  We&apos;re excited that you are participating in the UGA Cadathon!
                   We would love to see you at the event!
                 </p>
                 <p className="text-md">
